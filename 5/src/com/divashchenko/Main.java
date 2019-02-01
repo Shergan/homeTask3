ff0d75d6943ad830bb247b601d468b2d00de97ab
Dmitriy Ivashchenko <dmitriy.ivashchenko.work@gmail.com>
2019-02-01 18:26:12 +0200

    Initial commit

---
+++ 5/src/com/divashchenko/Main.java
@@ -0,0 +1,14 @@
+package com.divashchenko;
+
+/*2. Есть схема:
+        dl.dropboxusercontent.com/s/6fnasg6j3o2chdn/crystalcell.png
+        На ней 8 точек и 2 красных маркера. Каждый ход каждый из маркеров прыгает на случайную из соседних точек.
+        Игра заканчивается когда оба красных маркера оказывается в одной точке.
+        Вывести вероятность того что игра закончится на 1, 2...N ходу.*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 7/src/com/divashchenko/Main.java
@@ -0,0 +1,11 @@
+package com.divashchenko;
+
+/*4. Задан круг с радиусом r с центром в точке (x,y).
+        Вернуть список точек с целочисленными координатами которые попадают в круг.*/
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
