e8155ccee58541fad05124bc682a8286eae6069e
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-02-02 13:33:23 +0200

    task solved

--- 5/src/com/divashchenko/Main.java
+++ 5/src/com/divashchenko/Main.java
@@ -6,9 +6,88 @@ package com.divashchenko;
         Игра заканчивается когда оба красных маркера оказывается в одной точке.
         Вывести вероятность того что игра закончится на 1, 2...N ходу.*/
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
+    private static Random random = new Random();
 
     public static void main(String[] args) {
+        chanceOfTheGame();
+    }
 
+    private static int dotGame() {
+
+        String dotA = "twoCenter";
+        String dotB = "fourCenter";
+        int count = 0;
+
+        while (!dotA.equals(dotB)) {
+            dotA = whereIsDot(dotA);
+            dotB = whereIsDot(dotB);
+            count++;
+        }
+
+        return count;
+    }
+
+    private static String whereIsDot(String dot) {
+        if (dot.equals("oneUp") || dot.equals("oneDown")) {
+            return "twoCenter";
+        } else if (dot.equals("twoCenter")) {
+            int tmp = random.nextInt(3);
+            if (tmp == 0) {
+                return "oneUp";
+            } else if (tmp == 1) {
+                return "oneDown";
+            } else if (tmp == 2) {
+                return "threeUp";
+            } else {
+                return "threeDown";
+            }
+        } else if (dot.equals("threeUp") || dot.equals("threeDown")) {
+            int tmp = random.nextInt(2);
+            if (tmp == 0) {
+                return "twoCenter";
+            } else {
+                return "fourCenter";
+            }
+        } else if (dot.equals("fourCenter")) {
+            int tmp = random.nextInt(3);
+            if (tmp == 0) {
+                return "fiveUp";
+            } else if (tmp == 1) {
+                return "fiveDown";
+            } else if (tmp == 2) {
+                return "threeUp";
+            } else {
+                return "threeDown";
+            }
+        } else {
+            return "fourCenter";
+        }
+    }
+
+    private static void chanceOfTheGame() {
+        int[] arr = new int[1000];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = dotGame();
+        }
+
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int i : arr) {
+            if (counts.containsKey(i)) {
+                counts.put(i, counts.get(i) + 1);
+            } else {
+                counts.put(i, 1);
+            }
+        }
+
+        for (int i : counts.keySet()) {
+            System.out.println(i + " - " + (counts.get(i) * 100.0 / arr.length) + "%");
+        }
     }
 }
